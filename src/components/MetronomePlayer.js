ca92a8b50c5a9df6d39a9432dddaac63cf784615
Catrin <catrinhughes92@gmail.com>
2020-01-29 11:27:46 +0000

    onChange function

--- src/components/MetronomePlayer.js
+++ src/components/MetronomePlayer.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import MetronomeBox from '../containers/MetronomeBox';
 
-const MetronomePlayer = (props) => {
+const MetronomePlayer = ({onPlay, handleSlide}) => {
+
+    function onChange(e) {
+        handleSlide(e.target.value);
+    }
 
     return(
-        <button onClick={props.onPlay}>Click</button>
+        <>
+        <button onClick={onPlay}>Click</button>
+        <input type="range" min="40" max="218" onChange={onChange}></input>
+        </>
     )
 
 }
